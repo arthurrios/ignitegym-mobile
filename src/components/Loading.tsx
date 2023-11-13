f9a5c9a56711679815a0f77f0e5d2a58f47a12cd
arthur <arthur.rios007@gmail.com>
2023-11-13 18:23:39 -0300

    add theme

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,8 +2,8 @@ import { Spinner, Center } from 'native-base'
 
 export function Loading() {
   return (
-    <Center style={{ flex: 1 }}>
-      <Spinner />
+    <Center style={{ flex: 1 }} bg="gray.700">
+      <Spinner color="green.500" />
     </Center>
   )
 }
