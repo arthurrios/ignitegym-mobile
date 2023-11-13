8a7ce6eb6e2a4b7d89621b090fb12c3d1e0b2add
arthur <arthur.rios007@gmail.com>
2023-11-13 18:18:15 -0300

    add loading component

--- App.tsx
+++ App.tsx
@@ -1,29 +1,24 @@
 /* eslint-disable camelcase */
-import { StatusBar, Text, View } from 'react-native'
+import { StatusBar } from 'react-native'
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto'
+import { NativeBaseProvider } from 'native-base'
+import { Loading } from '@components/Loading'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#202024',
-      }}
-    >
+    <NativeBaseProvider>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Text>Hello World!</Text> : <View />}
-    </View>
+      <Loading />
+    </NativeBaseProvider>
   )
 }
