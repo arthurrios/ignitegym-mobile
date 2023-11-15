ebb904543ed4fffe4951991608e17517f1419900
arthur <arthur.rios007@gmail.com>
2023-11-15 17:11:08 -0300

    add section list to history

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,15 +1,44 @@
 import { HistoryCard } from '@components/HistoryCard'
 import { ScreenHeader } from '@components/ScreenHeader'
-import { VStack } from 'native-base'
+import { Heading, VStack, SectionList, Text } from 'native-base'
+import { useState } from 'react'
 
 export function History() {
+  const [exercises, setExercises] = useState([
+    {
+      title: '08.26.23',
+      data: ['Pull-down', 'One-arm dumbbel row'],
+    },
+    {
+      title: '08.27.23',
+      data: ['Pull-down'],
+    },
+  ])
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Exercise history" />
 
-      <HistoryCard />
-      <HistoryCard />
-      <HistoryCard />
+      <SectionList
+        sections={exercises}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <HistoryCard />}
+        renderSectionHeader={({ section }) => (
+          <Heading color="gray.200" fontSize="md" mt={10} mb={3}>
+            {section.title}
+          </Heading>
+        )}
+        px={8}
+        contentContainerStyle={
+          exercises.length === 0 && { flex: 1, justifyContent: 'center' }
+        }
+        ListEmptyComponent={() => (
+          <Text color="gray.100" textAlign="center">
+            There are no exercises registered yet. {'\n'}
+            Let's exercise?
+          </Text>
+        )}
+      />
     </VStack>
   )
 }
