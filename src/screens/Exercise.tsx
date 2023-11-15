868b2f97b1cc807a08adf0d71d7d12926a0c941c
arthur <arthur.rios007@gmail.com>
2023-11-15 18:07:05 -0300

    complete exercise screen

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,9 +1,12 @@
-import { HStack, Heading, Icon, Text, VStack } from 'native-base'
+import { Box, HStack, Heading, Icon, Image, Text, VStack } from 'native-base'
 import { TouchableOpacity } from 'react-native'
 import { Feather } from '@expo/vector-icons'
 import { useNavigation } from '@react-navigation/native'
 import { AppNavigatorRoutesProps } from '@routes/app.routes'
 import BodySvg from '@assets/body.svg'
+import SeriesSvg from '@assets/series.svg'
+import RepetitionsSvg from '@assets/repetitions.svg'
+import { Button } from '@components/Button'
 
 export function Exercise() {
   const navigation = useNavigation<AppNavigatorRoutesProps>()
@@ -41,6 +44,45 @@ export function Exercise() {
             </Text>
           </HStack>
         </HStack>
+      </VStack>
+
+      <VStack p={8}>
+        <Image
+          w="full"
+          h={80}
+          source={{
+            uri: 'https://hips.hearstapps.com/hmg-prod/images/form-check-index-1591205064.png?crop=0.888888888888889xw:1xh;center,top&resize=1200:*',
+          }}
+          alt="Exercise name"
+          mb={3}
+          resizeMode="cover"
+          rounded="lg"
+          overflow="hidden"
+        />
+
+        <Box bg="gray.600" rounded="md" pb={4} px={4}>
+          <HStack
+            alignItems="center"
+            justifyContent="space-around"
+            mb={6}
+            mt={5}
+          >
+            <HStack alignItems="center">
+              <SeriesSvg />
+              <Text color="gray.200" ml="2">
+                3 sets
+              </Text>
+            </HStack>
+
+            <HStack alignItems="center">
+              <RepetitionsSvg />
+              <Text color="gray.200" ml="2">
+                12 reps
+              </Text>
+            </HStack>
+          </HStack>
+          <Button title="Mark as done" />
+        </Box>
       </VStack>
     </VStack>
   )
