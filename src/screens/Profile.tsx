3fb197edaacb5634922dd990e6fb677635658059
arthur <arthur.rios007@gmail.com>
2023-11-15 17:31:23 -0300

    complete profile screen

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,17 +1,27 @@
+import { Button } from '@components/Button'
+import { Input } from '@components/Input'
 import { ScreenHeader } from '@components/ScreenHeader'
 import { UserPhoto } from '@components/UserPhoto'
-import { Center, ScrollView, Skeleton, VStack } from 'native-base'
+import {
+  Center,
+  Heading,
+  ScrollView,
+  Skeleton,
+  Text,
+  VStack,
+} from 'native-base'
 import { useState } from 'react'
+import { TouchableOpacity } from 'react-native'
 
 const PHOTO_SIZE = 33
 
 export function Profile() {
-  const [photoIsLoading, setPhotoIsLoading] = useState(true)
+  const [photoIsLoading, setPhotoIsLoading] = useState(false)
 
   return (
     <VStack flex={1}>
       <ScreenHeader title="Profile" />
-      <ScrollView>
+      <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
         <Center mt={6} px={10}>
           {photoIsLoading ? (
             <Skeleton
@@ -28,6 +38,46 @@ export function Profile() {
               size={PHOTO_SIZE}
             />
           )}
+          <TouchableOpacity>
+            <Text
+              color="green.500"
+              fontFamily="heading"
+              fontSize="md"
+              mt={2}
+              mb={8}
+            >
+              Change photo
+            </Text>
+          </TouchableOpacity>
+
+          <Input placeholder="Name" bg="gray.600" />
+          <Input
+            placeholder="E-mail"
+            bg="gray.600"
+            value="arthur.rios007@gmail.com"
+            isDisabled
+          />
+
+          <Heading
+            fontFamily="heading"
+            color="gray.200"
+            fontSize="md"
+            alignSelf="flex-start"
+            mb={2}
+            mt={12}
+          >
+            Change password
+          </Heading>
+
+          <Input placeholder="Old password" bg="gray.600" secureTextEntry />
+          <Input placeholder="New password" bg="gray.600" secureTextEntry />
+          <Input
+            placeholder="Confirm new password"
+            bg="gray.600"
+            secureTextEntry
+          />
+
+          <Button title="Update" mt={4} />
         </Center>
       </ScrollView>
     </VStack>
