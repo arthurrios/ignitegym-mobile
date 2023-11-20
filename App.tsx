084fa551c5f2d478347fb74250389e0900368ed7
arthur <arthur.rios007@gmail.com>
2023-11-20 12:28:47 -0300

    merge context provider

--- App.tsx
+++ App.tsx
@@ -9,7 +9,7 @@ import { NativeBaseProvider } from 'native-base'
 import { THEME } from './src/theme'
 import { Loading } from '@components/Loading'
 import { Routes } from '@routes/index'
-import { AuthContext } from '@contexts/AuthContext'
+import { AuthContextProvider } from '@contexts/AuthContext'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
@@ -21,18 +21,9 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      <AuthContext.Provider
-        value={{
-          user: {
-            id: '1',
-            name: 'Arthur',
-            email: 'arthur@email.com',
-            avatar: 'arthur.png',
-          },
-        }}
-      >
+      <AuthContextProvider>
         {fontsLoaded ? <Routes /> : <Loading />}
-      </AuthContext.Provider>
+      </AuthContextProvider>
     </NativeBaseProvider>
   )
 }
--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,10 +1,31 @@
 import { UserDTO } from '@dtos/UserDTO'
-import { createContext } from 'react'
+import { ReactNode, createContext } from 'react'
 
 export type AuthContextDataProps = {
   user: UserDTO
 }
 
+type AuthContextProviderProps = {
+  children: ReactNode
+}
+
 export const AuthContext = createContext<AuthContextDataProps>(
   {} as AuthContextDataProps,
 )
+
+export function AuthContextProvider({ children }: AuthContextProviderProps) {
+  return (
+    <AuthContext.Provider
+      value={{
+        user: {
+          id: '1',
+          name: 'Arthur Rios',
+          email: 'arthur@email.com',
+          avatar: 'arthur.png',
+        },
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  )
+}
