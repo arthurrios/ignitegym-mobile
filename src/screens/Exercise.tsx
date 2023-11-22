88d248f97cb74820e7ad84b7f80b4fce40efb473
arthur <arthur.rios007@gmail.com>
2023-11-22 10:09:17 -0300

    fetch and show exercise data

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -7,22 +7,66 @@ import {
   ScrollView,
   Text,
   VStack,
+  useToast,
 } from 'native-base'
 import { TouchableOpacity } from 'react-native'
 import { Feather } from '@expo/vector-icons'
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation, useRoute } from '@react-navigation/native'
 import { AppNavigatorRoutesProps } from '@routes/app.routes'
 import BodySvg from '@assets/body.svg'
 import SeriesSvg from '@assets/series.svg'
 import RepetitionsSvg from '@assets/repetitions.svg'
 import { Button } from '@components/Button'
+import { AppError } from '@utils/AppError'
+import { api } from '@services/api'
+import { useEffect, useState } from 'react'
+import { ExerciseDTO } from '@dtos/ExerciseDTO'
+import { Loading } from '@components/Loading'
+
+type RoutesParamsProps = {
+  exerciseId: string
+}
 
 export function Exercise() {
+  const [isLoading, setIsLoading] = useState(true)
+  const [exercise, setExercise] = useState<ExerciseDTO>({} as ExerciseDTO)
+
   const navigation = useNavigation<AppNavigatorRoutesProps>()
+
+  const toast = useToast()
+
+  const route = useRoute()
+
+  const { exerciseId } = route.params as RoutesParamsProps
 
   function handleGoBack() {
     navigation.goBack()
   }
+
+  async function fetchExerciseDetails() {
+    try {
+      setIsLoading(true)
+      const response = await api.get(`/exercises/${exerciseId}`)
+      setExercise(response.data)
+    } catch (error) {
+      const isAppError = error instanceof AppError
+      const title = isAppError
+        ? error.message
+        : 'Error fetching exercise details'
+
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: 'red.500',
+      })
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchExerciseDetails()
+  }, [exerciseId])
 
   return (
     <VStack flex={1}>
@@ -43,24 +87,27 @@ export function Exercise() {
             size="lg"
             flexShrink={1}
           >
-            Pull-down
+            {exercise.name}
           </Heading>
 
           <HStack alignItems="center">
             <BodySvg />
             <Text color="gray.200" ml={1} textTransform="capitalize">
-              Back
+              {exercise.group}
             </Text>
           </HStack>
         </HStack>
       </VStack>
-      <ScrollView>
+
+      {isLoading ? (
+        <Loading />
+      ) : (
         <VStack p={8}>
           <Image
             w="full"
             h={80}
             source={{
-              uri: 'https://hips.hearstapps.com/hmg-prod/images/form-check-index-1591205064.png?crop=0.888888888888889xw:1xh;center,top&resize=1200:*',
+              uri: `${api.defaults.baseURL}/exercise/demo/${exercise.demo}`,
             }}
             alt="Exercise name"
             mb={3}
@@ -79,21 +126,21 @@ export function Exercise() {
               <HStack alignItems="center">
                 <SeriesSvg />
                 <Text color="gray.200" ml="2">
-                  3 sets
+                  {exercise.series} sets
                 </Text>
               </HStack>
 
               <HStack alignItems="center">
                 <RepetitionsSvg />
                 <Text color="gray.200" ml="2">
-                  12 reps
+                  {exercise.repetitions} reps
                 </Text>
               </HStack>
             </HStack>
             <Button title="Mark as done" />
           </Box>
         </VStack>
-      </ScrollView>
+      )}
     </VStack>
   )
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -19,8 +19,8 @@ export function Home() {
   const toast = useToast()
   const navigation = useNavigation<AppNavigatorRoutesProps>()
 
-  function handleOpenExerciseDetails() {
-    navigation.navigate('exercise')
+  function handleOpenExerciseDetails(exerciseId: string) {
+    navigation.navigate('exercise', { exerciseId })
   }
 
   async function fetchGroups() {
@@ -109,7 +109,10 @@ export function Home() {
             data={exercises}
             keyExtractor={(item) => item.id}
             renderItem={({ item }) => (
-              <ExerciseCard onPress={handleOpenExerciseDetails} data={item} />
+              <ExerciseCard
+                onPress={() => handleOpenExerciseDetails(item.id)}
+                data={item}
+              />
             )}
             showsVerticalScrollIndicator={false}
             _contentContainerStyle={{ paddingBottom: 20 }}
