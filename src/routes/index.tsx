d088722a708fc96c0a3b78bb7f4244e2227f0542
arthur <arthur.rios007@gmail.com>
2024-01-20 18:41:56 -0300

    chore: send notification data

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,12 +4,16 @@ import { Box, useTheme } from 'native-base'
 import { AppRoutes } from './app.routes'
 import { useAuth } from '@hooks/useAuth'
 import { Loading } from '@components/Loading'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { tagIsLoggedIn } from '@notifications/notificationTags'
+import { OSNotification, OneSignal } from 'react-native-onesignal'
+import { NotificationEventTypeMap } from 'react-native-onesignal/dist/models/NotificationEvents'
+import { Notification } from '@components/Notification'
 
 export function Routes() {
   const { colors } = useTheme()
   const { user, isLoadingUserStorageData } = useAuth()
+  const [notification, setNotification] = useState<OSNotification>()
 
   const theme = DefaultTheme
   theme.colors.background = colors.gray[700]
@@ -17,6 +21,21 @@ export function Routes() {
   useEffect(() => {
     tagIsLoggedIn(user.id ? 'true' : 'false')
   }, [user])
+
+  useEffect(() => {
+    const unsubscribe = OneSignal.Notifications.addEventListener(
+      'foregroundWillDisplay',
+      (
+        notificationReceivedEvent: NotificationEventTypeMap['foregroundWillDisplay'],
+      ) => {
+        const response = notificationReceivedEvent.getNotification()
+
+        setNotification(response)
+      },
+    )
+
+    return () => unsubscribe
+  }, [])
 
   if (isLoadingUserStorageData) {
     return <Loading />
@@ -26,6 +45,12 @@ export function Routes() {
     <Box flex={1} bg="gray.700">
       <NavigationContainer>
         {user.id ? <AppRoutes /> : <AuthRoutes />}
+        {notification?.title && (
+          <Notification
+            data={notification}
+            onClose={() => setNotification(undefined)}
+          />
+        )}
       </NavigationContainer>
     </Box>
   )
